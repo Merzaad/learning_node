4fbcb64dd11c43eb03f735fae94b609ac6bb0766
Merzaad <Merzaad@gmail.com>
2022-08-13 19:37:50 +0430

    add package.json

--- app.mjs
+++ app.mjs
@@ -1,3 +1,6 @@
-import { getName } from './module.mjs'
+import { getName, getDir, getOsUptimeInHr } from './module.mjs'
 
 getName().then((result) => console.log(result))
+const dirname = getDir('/node/module.mjs')
+const uptime = getOsUptimeInHr()
+console.log(uptime)
--- module.mjs
+++ module.mjs
@@ -1,10 +1,19 @@
+import path from 'path'
+import os from 'os'
+
 const getName = async () => {
   const promise = () =>
     new Promise((resolve) => {
-      setTimeout(() => resolve('M'), 2000)
+      setTimeout(() => resolve('M'), 1000)
     })
   const result = await promise()
   return result
 }
 
-export { getName }
+const getDir = (pathString) => path.dirname(pathString)
+const getOsUptimeInHr = () => {
+  const uptimInSec = os.uptime()
+  return `uptime: ${Math.floor(uptimInSec / 3600)}hrs`
+}
+
+export { getName, getDir, getOsUptimeInHr }
