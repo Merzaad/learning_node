a92bf121dd49da016da47c693e0bcdeaa382b0a5
Merzaad <Merzaad@gmail.com>
2022-08-19 16:42:12 +0430

    add favicon response

--- server.mjs
+++ server.mjs
@@ -9,17 +9,22 @@ const PORT = process.env.PORT || 420
 // const favicon = path.join(__dirname, 'node.png')
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { status: '200' })
   if (request.method === 'GET' && request.url === '/favicon.ico') {
-    response.writeHead(204, { status: 'todo' })
+    response.writeHead(200, {'Content-Type' : 'image/png'});
+    fs.readFile('node.png', (error, data) => {
+      response.write(data)
+      response.end()
+    })
+  } else {
+    response.writeHead(200, { status: '200' })
+    const learningData = {
+      url: request.url,
+      method: request.method,
+      query: url.parse(request.url, true).query,
+    }
+    response.write(JSON.stringify(learningData, null, 1))
+    response.end()
   }
-  const learningData = {
-    url: request.url,
-    method: request.method,
-    query: url.parse(request.url, true).query
-  }
-  response.write(JSON.stringify(learningData, null, 1))
-  response.end()
 })
 
 server.listen(PORT, () => console.log(`server running on ${PORT}`))
