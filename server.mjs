c6a026125481c1108a0b0c0cfc2a2a2f18e61dcb
Merzaad <Merzaad@gmail.com>
2022-08-19 12:07:59 +0430

    read query

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import url from 'url'
 import fs from 'fs'
 import path from 'path'
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+// const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 const PORT = process.env.PORT || 420
 // const favicon = path.join(__dirname, 'node.png')
@@ -11,9 +11,15 @@ const PORT = process.env.PORT || 420
 const server = http.createServer((request, response) => {
   response.writeHead(200, { status: '200' })
   if (request.method === 'GET' && request.url === '/favicon.ico') {
-    response.writeHead(204, { 'status': 'todo' })
+    response.writeHead(204, { status: 'todo' })
   }
-  response.end('end')
+  const learningData = {
+    url: request.url,
+    method: request.method,
+    query: url.parse(request.url, true).query
+  }
+  response.write(JSON.stringify(learningData, null, 1))
+  response.end()
 })
 
 server.listen(PORT, () => console.log(`server running on ${PORT}`))
